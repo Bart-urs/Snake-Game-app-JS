26fd4d9cf4e090fd4089c5d1f2fed7282fd6b605
Iris de la Luna <iris@random.town>
2023-11-21 16:05:45 +0100

    Show last recorded score

--- app.js
+++ app.js
@@ -4,15 +4,26 @@ const express = require('express')
 const app = express()
 const port = 4000
 const currentDirectory = process.cwd();
+const scoresFile = path.join(currentDirectory, "scores.json");
 
 app.use(express.json());
 
-app.get('/scores', (req, res) => {
-  // TODO
+app.get('/scores', async (req, res) => {
+  const scores = await getLastScore(scoresFile)
+  res.send(scores);
 })
 
+async function getLastScore(fileName) {
+  try {
+    console.log('Reading scores from file:', fileName);
+    return await fs.readFile(fileName)
+  } catch(err) {
+    console.error("Error reading from file:", err)
+  }
+}
+
 app.post("/scores", async (req, res) => {
-  await updateScores(req.body, path.join(currentDirectory, "scores.json"))
+  await updateScores(req.body, scoresFile)
   res.send("OK!")
 })
 
